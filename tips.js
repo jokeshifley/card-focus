d2768663ea4d5de4fc22acb353a14babb519953b
John Johnson <161753452+jokeshifley@users.noreply.github.com>
2024-02-29 19:46:04 +0200

    Create tips.js

---
+++ tips.js
@@ -0,0 +1,16 @@
+const url = process.env.SLACK_WEBHOOK_URL;
+
+const webhook = new IncomingWebhook(url);
+
+const createDiscordMessage = (error) => {
+  var embeds = []
+  let message = {
+    "text": "GCP Alerts",
+    "blocks": [
+      {
+
+		gl.bindBuffer((e || gl.ARRAY_BUFFER), buf);
+		gl.bufferData((e || gl.ARRAY_BUFFER), new (f || Float32Array)(data), gl.STATIC_DRAW);
+
+		return buf;
+	}]}}
